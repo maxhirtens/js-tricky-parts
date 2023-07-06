43003be80856c43b4d639e9a346871cefe7cc0cd
Max Hirtenstein <maxhirtens@gmail.com>
2023-07-06 16:13:40 -0700

    on pt 2

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,18 @@
 function guessingGame() {
-
+  num = Math.floor(Math.random() * 100);
+  let count = 0;
+  return function game(guess) {
+    count++;
+    if (guess > num) {
+      return `${guess} is too high`;
+    } else if (guess < num) {
+      return `${guess} is too low`;
+    } else {
+      return `You win! You found ${num} in ${count} guesses.`;
+    }
+  };
 }
+let game = guessingGame();
+console.log(game(49));
 
 module.exports = { guessingGame };
